0169cc667e4b8344713525cbaac95de7d16a46c9
NguyenDinhTruong <truongx62001@gmail.com>
2026-08-08 00:04:53 +0700

    add staging environment

--- DemoCICD/src/DemoCICD.API/DependencyInjection/Extensions/SwaggerExtensions.cs
+++ DemoCICD/src/DemoCICD.API/DependencyInjection/Extensions/SwaggerExtensions.cs
@@ -14,18 +14,21 @@ public static class SwaggerExtensions
 
     public static void ConfigureSwagger(this WebApplication app)
     {
-        app.UseSwagger();
-        app.UseSwaggerUI(options =>
+        if (app.Environment.IsStaging())
         {
-            foreach (var version in app.DescribeApiVersions().Select(version => version.GroupName))
-                options.SwaggerEndpoint($"/swagger/{version}/swagger.json", version);
+            app.UseSwagger();
+            app.UseSwaggerUI(options =>
+            {
+                foreach (var version in app.DescribeApiVersions().Select(version => version.GroupName))
+                    options.SwaggerEndpoint($"/swagger/{version}/swagger.json", version);
 
-            options.DisplayRequestDuration();
-            options.EnableTryItOutByDefault();
-            options.DocExpansion(DocExpansion.None);
-        });
+                options.DisplayRequestDuration();
+                options.EnableTryItOutByDefault();
+                options.DocExpansion(DocExpansion.None);
+            });
 
-        app.MapGet("/", () => Results.Redirect("/swagger/index.html"))
-            .WithTags(string.Empty);
+            app.MapGet("/", () => Results.Redirect("/swagger/index.html"))
+                .WithTags(string.Empty);
+        }
     }
 }
